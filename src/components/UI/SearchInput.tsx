f1e63e5a44c9dff948dc722ee2b7f602b6c33385
wigya <amajewski505@gmail.com>
2024-05-14 22:49:48 +0200

    add debouncing

--- src/components/UI/SearchInput.tsx
+++ src/components/UI/SearchInput.tsx
@@ -1,17 +1,25 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Input } from "antd";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { FilterContext } from "../../context/FilterContext";
 
 const SearchInput: React.FC = () => {
   const filterContext = useContext(FilterContext);
-  // COMMENT dodac debounce zamiast caly czas onChange rerenderowac wszystkie kompnenety
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let timer = 0;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      filterContext?.setFilterSearchQuery(e.target.value);
+    }, 500);
+  };
+
   return (
     <Input
       prefix={<FontAwesomeIcon icon={faSearch} />}
       className="w-1/3"
-      onChange={(e) => filterContext?.setFilterSearchQuery(e.target.value)}
+      onChange={(e) => handleOnChange(e)}
     />
   );
 };
